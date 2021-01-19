1859a74d73cbbc7a500b1099fbe80142276299c4
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-01-19 17:22:48 -0300

    CalculadoraFlex
    A atividade Calculadora Flex foi Concluida com Consucesso

--- Javascript/Aplicativo Calculadora Flex/calculadora.js
+++ Javascript/Aplicativo Calculadora Flex/calculadora.js
@@ -1,6 +1,18 @@
-var etanol,gasolina;
+var etanol;
+var gasolina;
 
-function calcular{
+function calcular(){
+    etanol=parseFloat(frmFlex.txtEtanol.value);
+    gasolina=parseFloat(frmFlex.txtGasolina.value);
 
-
+    if(etanol <0.7 * gasolina) {
+       document.getElementById("status").src="img/etanol.png";
+    }else{
+        document.getElementById("status").src="img/gasolina.png";
+    }
 }
+
+function resetar(){
+    document.getElementById("status").src="img/neutro.png"
+
+} 
