7f06abd04ea4d5b5eb0ddbc15f0394ab7f09dc67
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-01-21 22:14:41 -0300

    Jododedados
    Feito o jogo de Dados

---
+++ Javascript/Jogado De Dado/java.js
@@ -0,0 +1,22 @@
+var sorteio = Math.floor(Math.random() * 6 + 1);
+
+switch(sorteio){
+    case 1:
+        document.getElementById("face").src = "img_dados/dado1.png";
+    break;
+    case 2:
+        document.getElementById("face").src = "img_dados/dado2.png";
+    break;
+    case 3:
+        document.getElementById("face").src = "img_dados/dado3.png";
+    break;       
+    case 4:
+        document.getElementById("face").src = "img_dados/dado4.png";
+    break;
+    case 5:
+        document.getElementById("face").src = "img_dados/dado5.png";
+    break;       
+    case 6:
+        document.getElementById("face").src = "img_dados/dado6.png";
+    break;                          
+}
