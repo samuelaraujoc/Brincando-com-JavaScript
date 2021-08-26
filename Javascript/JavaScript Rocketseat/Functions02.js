38a837465926b11dd7d5d6777ab0d3cac2c3597f
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-08-25 21:24:20 -0300

    Funções
    Aprendendo funções em JavaScript, com explicações  Lúdicas

---
+++ Javascript/JavaScript Rocketseat/Functions02.js
@@ -0,0 +1,18 @@
+//xemplos de function expression ou Anonymous
+// Função com parametro
+const sum = function(number1, number2){
+    console.log(number1 + number2)
+}
+sum (2, 3) // arguments
+sum (8, 8)
+
+
+//Explicação sobre função de uma forma Ludica
+//Função é como um liquidificador
+
+function fazersuco (fruta1, fruta2){
+    return fruta1 + fruta2
+}
+const copo = fazersuco('banana', 'maça')
+
+console.log(copo)
