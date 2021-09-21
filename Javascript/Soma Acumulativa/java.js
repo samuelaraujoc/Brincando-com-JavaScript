8fb88fcead97c97e489c980748cd791353026e8b
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-09-21 14:49:24 -0300

    Revert "Somandodoisvalores"
    This reverts commit 1d8b74b021c7c8469f76a4c20962ad10fc8a2606.

--- Javascript/Soma Acumulativa/java.js
+++
@@ -1,6 +0,0 @@
-function somar() {
-    var label = document.getElementById('1');
-    var input = document.getElementById('2');
-    label.innerHTML = parseInt(input.value) + parseInt(label.textContent);
-
-  }
