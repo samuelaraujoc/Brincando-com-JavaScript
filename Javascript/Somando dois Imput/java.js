b287775dfd7a0331b81fc36e30bfb6f7608cabc2
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-09-21 22:11:51 -0300

    Deletetr
    Testecomit

--- Javascript/Somando dois Imput/java.js
+++
@@ -1,8 +0,0 @@
-function clicar() {
-    const num1 = document.querySelector(".num1").value;
-    const resultado = parseInt(num1)
-    document.querySelector(".resultado").innerHTML = resultado;
-}
-
-
-
