715215a90171fbdecf3d61a482e5972ecc12eacf
Samuel Araujo <53999532+samuelsam8@users.noreply.github.com>
2021-09-21 22:08:27 -0300

    Revert "somandoimput"
    This reverts commit e4aa74abf6b61377a8c7b08d296db8f4e8571b1a.

---
+++ Javascript/Somando dois Imput/java.js
@@ -0,0 +1,8 @@
+function clicar() {
+    const num1 = document.querySelector(".num1").value;
+    const resultado = parseInt(num1)
+    document.querySelector(".resultado").innerHTML = resultado;
+}
+
+
+
