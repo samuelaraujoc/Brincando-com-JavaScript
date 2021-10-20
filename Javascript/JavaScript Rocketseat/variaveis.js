ab724f3c65902d491f65cd6094adae06988002a9
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2021-10-20 19:56:14 -0300

    organizando repositorio

--- Javascript/JavaScript Rocketseat/variaveis.js
+++
@@ -1,6 +0,0 @@
-//(var) var é global/local e podera funcionar fora de um escopo de bloco. Obs: no java script moderno é evitado usar Var para declarara variaveis
-
-//(let) e (const) são locais e só funcionam no escopo onde foi criada.
-
-
-//(const) Variavel constante não pode ter seu tipo  alterada.
