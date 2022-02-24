828c559b343613a446cc634cfb4d3483220c35c6
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2022-02-24 16:41:49 -0300

    PreprandoApp_1
    Telas principais

---
+++ React Native/Nevigation/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SafeAreaView, Text} from 'react-native'
+import TextoCentral from './components/TextoCentral'
+
+export default props => (
+     <SafeAreaView style={{flex: 1}}>
+         <TextoCentral>App</TextoCentral>
+     </SafeAreaView>
+)
