ee00469f4c7b1e2daaf2e7790b7cad200e650cf0
Samuel Araujo <samuel.araujo.carvalho@gmail.com>
2022-03-08 17:31:08 -0300

    Clean
    Limpando GIT

--- React Native/Nevigation/src/views/TelaC.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import TextoCentral from '../components/TextoCentral'
-
-export default props => (
-        <TextoCentral corFundo='#9932cd'>
-            Tela C
-        </TextoCentral>
-)
